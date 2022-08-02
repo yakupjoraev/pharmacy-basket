08f66c2411c4a241442ac10fc4576f821ffbbf9d
Yakup <yakupjoraev@gmail.com>
2022-08-02 18:46:30 +0300

    страницы no-search-result

--- js/no-result-parallax.js
+++ js/no-result-parallax.js
@@ -64,4 +64,68 @@ window.onload = function () {
       coordYprocent = coordY / parallaxHeight * 100;
     });
   }
+};
+
+window.onload = function () {
+  const parallax = document.querySelector('.images-parallax-two');
+
+  if (parallax) {
+    const parallaxBody = document.querySelector('.images-parallax__body')
+    const parallaxDoctor = document.querySelector('.images-parallax__doctor')
+    const parallaxPillRedTwo = document.querySelector('.images-parallax__pill-red-two')
+    const parallaxPillRedOne = document.querySelector('.images-parallax__pill-red-one')
+    const parallaxPillGreenTwo = document.querySelector('.images-parallax__pill-green-two')
+    const parallaxPillGreenOne = document.querySelector('.images-parallax__pill-green-one')
+    
+    // Коэффициенты
+    const forDoctor = 40;
+    const forPillRedTwo = 20;
+    const forPillRedOne = 30;
+    const forPillGreenTwo = 40;
+    const forPillGreenOne = 40;
+
+    // Скорость анимации
+    const speed = 0.05;
+
+    // Объявление переменных
+
+    let positionX = 0, positionY = 0;
+    let coordXprocent = 0, coordYprocent = 0;
+
+    function setMauseParallaxStyle() {
+      const distX = coordXprocent - positionX;
+      const distY = coordYprocent - positionY;
+
+      positionX = positionX + (distX * speed);
+      positionY = positionY + (distY * speed);
+
+      // Передаем стили
+      parallaxDoctor.style.cssText = `transform: translate(${positionX / forDoctor}%,${positionY / forDoctor}%);`;
+
+      parallaxPillRedTwo.style.cssText = `transform: translate(${positionX / forPillRedTwo}%,${positionY / forPillRedTwo}%);`;
+
+      parallaxPillRedOne.style.cssText = `transform: translate(${positionX / forPillRedOne}%,${positionY / forPillRedOne}%);`;
+
+      parallaxPillGreenTwo.style.cssText = `transform: translate(${positionX / forPillGreenTwo}%,${positionY / forPillGreenTwo}%);`;
+
+      parallaxPillGreenOne.style.cssText = `transform: translate(${positionX / forPillGreenOne}%,${positionY / forPillGreenOne}%);`;
+
+      requestAnimationFrame(setMauseParallaxStyle);
+    }
+    setMauseParallaxStyle();
+
+    parallax.addEventListener('mousemove', function (e) {
+      // Получение ширины и высоты блока
+      const parallaxWidth = parallax.offsetWidth;
+      const parallaxHeight = parallax.offsetHeight;
+
+      // Ноль по середине 
+      const coordX = e.pageX - parallaxWidth / 2;
+      const coordY = e.pageY - parallaxHeight / 2;
+
+      // Получаем проценты 
+      coordXprocent = coordX / parallaxWidth * 100;
+      coordYprocent = coordY / parallaxHeight * 100;
+    });
+  }
 }
